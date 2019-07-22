d8d7db148ee5e877b8fd985cc025384ea5954ea1
Connor Shaughnessy <sapphon@gmail.com>
2019-07-21 23:23:17 -0400

    Magic Wand onItemRightClick signature and return type updates

--- src/main/java/org/sapphon/minecraft/modding/techmage/MagicWand.java
+++ src/main/java/org/sapphon/minecraft/modding/techmage/MagicWand.java
@@ -6,8 +6,11 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import org.sapphon.minecraft.modding.minecraftpython.IArcane;
 import org.sapphon.minecraft.modding.minecraftpython.ScriptLoaderConstants;
@@ -65,17 +68,17 @@ public class MagicWand extends Item implements IArcane {
 	}
 
 	@Override
-	public ItemStack onItemRightClick(ItemStack itemThatWasRightClicked,
-			World world, EntityPlayer techmage) {
+	public ActionResult<ItemStack> onItemRightClick(ItemStack itemThatWasRightClicked,
+													World world, EntityPlayer techmage, EnumHand hand) {
 		if (techmage.experienceLevel < this.experienceLevelRequirement) {
 			if (world.isRemote) {
-				techmage.addChatMessage(new ChatComponentText(
+				techmage.addChatMessage(new TextComponentString(
 						"Not enough experience!"));
 			}
 		} else if (timer() > this.getSpell().getCooldownInMilliseconds()) {
 			this.castSpellFromWand(techmage, world);
 		}
-		return itemThatWasRightClicked;
+		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemThatWasRightClicked);
 	}
 
 	private long timer() {
